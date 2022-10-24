9598884ef959d87bda4bbe00f956ae83f7ddadac
Haliyah Tijani <haliyya460@gmail.com>
2022-10-24 22:03:03 +0100

    upd

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const userRoutes = require('./routes/user-route');
+const postRoutes = require('./routes/posts')
+
+const app = express();
+const port = 3050;
+
+
+app.use(bodyParser.urlencoded({ extended : true }));
+app.use(bodyParser.json());
+app.use('/users',userRoutes)
+app.use('/posts',postRoutes)
+
+
+
+app.get('/', (req, res) => {
+    res.send("works lola")
+})
+
+
+
+app.listen(port, (err) => {
+    if (err) {
+        console.log("err", err);
+    } else {
+        console.log("listening", port);
+    }
+})
